e92794732d9e823548573e4ac6226a376538e2fa
Edu <egarcia262@gmail.com>
2022-03-21 15:53:53 +0000

    Add users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+  
   def new
     @user = User.new
   end
@@ -11,6 +16,20 @@ class UsersController < ApplicationController
       redirect_to articles_path
     else
       render 'new'
+    end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Your account information was successfully updated"
+      redirect_to articles_path
+    else
+      render 'edit'
     end
   end
 
